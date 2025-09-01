bfd1b7f955f446ef7c3b52e1b8ede57742dd2b72
azainal093 <azainal093@gmail.com>
2025-09-01 23:53:35 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,17 +2,17 @@ systemctl stop genieacs-{cwmp,ui,nbi}
 sleep 5
 mkdir /root/db
 cd /root/db
-wget https://github.com/azainal093/genieacs_windows/blob/main/config.bson
-wget https://github.com/azainal093/genieacs_windows/blob/main/config.metadata.json
-wget https://github.com/azainal093/genieacs_windows/blob/main/permissions.bson
-wget https://github.com/azainal093/genieacs_windows/blob/main/permissions.metadata.json
-wget https://github.com/azainal093/genieacs_windows/blob/main/presets.bson
-wget https://github.com/azainal093/genieacs_windows/blob/main/presets.metadata.json
-wget https://github.com/azainal093/genieacs_windows/blob/main/provisions.bson
-wget https://github.com/azainal093/genieacs_windows/blob/main/provisions.metadata.json
-wget https://github.com/azainal093/genieacs_windows/blob/main/users.bson
-wget https://github.com/azainal093/genieacs_windows/blob/main/users.metadata.json
-wget https://github.com/azainal093/genieacs_windows/blob/main/virtualParameters.bson
-wget https://github.com/azainal093/genieacs_windows/blob/main/virtualParameters.metadata.json
+wget https://raw.githubusercontent.com/azainal093/genieacs_windows/refs/heads/main/config.bson
+wget https://raw.githubusercontent.com/azainal093/genieacs_windows/refs/heads/main/config.metadata.json
+wget https://raw.githubusercontent.com/azainal093/genieacs_windows/refs/heads/main/permissions.bson
+wget https://raw.githubusercontent.com/azainal093/genieacs_windows/refs/heads/main/permissions.metadata.json
+wget https://raw.githubusercontent.com/azainal093/genieacs_windows/refs/heads/main/presets.bson
+wget https://raw.githubusercontent.com/azainal093/genieacs_windows/refs/heads/main/presets.metadata.json
+wget https://raw.githubusercontent.com/azainal093/genieacs_windows/refs/heads/main/provisions.bson
+wget https://raw.githubusercontent.com/azainal093/genieacs_windows/refs/heads/main/provisions.metadata.json
+wget https://raw.githubusercontent.com/azainal093/genieacs_windows/refs/heads/main/users.bson
+wget https://raw.githubusercontent.com/azainal093/genieacs_windows/refs/heads/main/users.metadata.json
+wget https://raw.githubusercontent.com/azainal093/genieacs_windows/refs/heads/main/virtualParameters.bson
+wget https://raw.githubusercontent.com/azainal093/genieacs_windows/refs/heads/main/virtualParameters.metadata.json
 mongorestore --db genieacs --drop /root/db
 systemctl start genieacs-{cwmp,ui,nbi}
